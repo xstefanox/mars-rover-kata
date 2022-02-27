8ee046e55f04967b087c3a355ee40c020bd3f30b
Stefano Varesi <stefano.varesi@gmail.com>
2022-02-27 14:23:30 +0100

    refactor: directly assign initial direction in constructor

--- src/main/kotlin/io/github/xstefanox/marsrover/MarsRover.kt
+++ src/main/kotlin/io/github/xstefanox/marsrover/MarsRover.kt
@@ -2,15 +2,13 @@ package io.github.xstefanox.marsrover
 
 import io.github.xstefanox.marsrover.Direction.N
 
-class MarsRover(x: Int = 0, y: Int = 0, direction: Direction = N) {
+class MarsRover(x: Int = 0, y: Int = 0, val direction: Direction = N) {
 
     var position: Position = Position(x, y)
         private set(value) {
             Position(value.x, value.y)
             field = value
         }
-
-    val direction: Direction = direction
 
     fun moveForward() {
         position = Position(0, 1)
